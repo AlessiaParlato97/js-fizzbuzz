27ec14eca63f7c668e6948292287f25d5e638c31
Alessia Parlato <alessiaparlato@hotmail.it>
2024-07-05 18:37:56 +0200

    ADD: JS

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
 console.log('FOR LOOP');
 
-for (let i = 0; i <= 100; i++) {
-    console.log(i);
+for (let i = 0; i < 100; i++) {
+    let num = i + 1;
+    console.log(num);
 }
+
+//condizioni
+for (let i = 0; i < 100; i++) {
+    let num3 = i * 3;
+    console.log(num3);
+    //console.log('Fizz');
+}
+
+for (let i = 0; i <= 100; i++) {
+    let num5 = i * 5;
+    console.log(num5);
+    //console.log('Buzz');
+}
+
+//multipli di 3 = Fizz
+
+//multipli di 5 = Buzz
+
+//multipli di 3&5 = FizzBuzz
